6697ca027c96fb570341aa1fc450528710a5f5b4
WangLongBiao <whlsbc@qq.com>
2020-11-16 19:14:49 +0800

    add auth2

---
+++ mist-gateway/src/test/java/com/wanglongbiao/mist/gateway/MistGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanglongbiao.mist.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
