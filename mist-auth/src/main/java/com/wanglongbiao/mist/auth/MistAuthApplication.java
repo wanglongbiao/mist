e1251d80f262e042f1ec29bcdc4fdd58b2992c93
WangLongBiao <whlsbc@qq.com>
2020-11-16 19:12:39 +0800

    add auth

---
+++ mist-auth/src/main/java/com/wanglongbiao/mist/auth/MistAuthApplication.java
@@ -0,0 +1,13 @@
+package com.wanglongbiao.mist.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MistAuthApplication.class, args);
+    }
+
+}
---
+++ mist-auth/src/test/java/com/wanglongbiao/mist/auth/MistAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanglongbiao.mist.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
