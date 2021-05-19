6d70cc3a7b421d322e01bf8e5e03ae575539a73f
wanglongbiao <whlsbc@qq.com>
2021-05-19 08:27:35 +0800

    test allocation

--- mist-auth/src/test/java/com/wanglongbiao/mist/auth/ch03/ReferenceCountingGC.java
+++ mist-auth/src/test/java/com/wanglongbiao/mist/auth/ch03/ReferenceCountingGC.java
@@ -15,7 +15,45 @@ public class ReferenceCountingGC {
 //        System.gc();
     }
 
+    /**
+     * -verbose:gc -XX:+PrintGCDetails -Xms20m -Xmx20m -Xmn10m
+     */
+    public static void testAllocation() {
+        System.out.println("1");
+        byte[] allocation1 = new byte[2 * _1MB];
+        System.out.println("2");
+        byte[] allocation2 = new byte[2 * _1MB];
+        System.out.println("3");
+        byte[] allocation3 = new byte[2 * _1MB];
+        System.out.println("4");
+        byte[] allocation4 = new byte[4 * _1MB];
+        System.out.println("5");
+        /**
+         * [0.002s][info][gc] Using G1
+         * [0.144s][info][gc] GC(0) Pause Young (Normal) (G1 Evacuation Pause) 10M->7M(20M) 3.451ms
+         * 1
+         * [0.147s][info][gc] GC(1) Pause Young (Concurrent Start) (G1 Humongous Allocation) 7M->7M(20M) 1.747ms
+         * [0.147s][info][gc] GC(2) Concurrent Cycle
+         * 2
+         * 3
+         * [0.148s][info][gc] GC(3) Pause Young (Normal) (G1 Humongous Allocation) 13M->13M(20M) 0.248ms
+         * [0.151s][info][gc] GC(4) Pause Full (G1 Humongous Allocation) 13M->13M(20M) 3.005ms
+         * [0.151s][info][gc] GC(2) Concurrent Cycle 4.206ms
+         * 4
+         * [0.152s][info][gc] GC(5) Pause Young (Concurrent Start) (G1 Humongous Allocation) 16M->16M(20M) 0.256ms
+         * [0.152s][info][gc] GC(6) Concurrent Cycle
+         * [0.152s][info][gc] GC(7) Pause Young (Normal) (G1 Humongous Allocation) 16M->16M(20M) 0.115ms
+         * [0.154s][info][gc] GC(8) Pause Full (G1 Humongous Allocation) 16M->16M(20M) 1.671ms
+         * [0.155s][info][gc] GC(9) Pause Full (G1 Humongous Allocation) 16M->16M(20M) 1.618ms
+         * [0.155s][info][gc] GC(6) Concurrent Cycle 3.683ms
+         * Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+         */
+    }
+
     public static void main(String[] args) {
-        ReferenceCountingGC.testGC();
+//        ReferenceCountingGC.testGC();
+        System.out.println("m1");
+        ReferenceCountingGC.testAllocation();
+        System.out.println("m2");
     }
 }
---
+++ mist-auth/src/test/java/com/wanglongbiao/mist/auth/ch03/ShowGc.java
@@ -0,0 +1,14 @@
+package com.wanglongbiao.mist.auth.ch03;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+public class ShowGc {
+    public static void main(String args[]) {
+        List<GarbageCollectorMXBean> l = ManagementFactory.getGarbageCollectorMXBeans();
+        for(GarbageCollectorMXBean b : l) {
+            System.out.println(b.getName());
+        }
+    }
+}
