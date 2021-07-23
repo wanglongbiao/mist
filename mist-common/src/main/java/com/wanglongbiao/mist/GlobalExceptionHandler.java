8353eaacc5d9ad66cf195030a54f92ef6adceb86
wanglongbiao <wanglongbiao@vip.qq.com>
2021-07-23 16:41:28 +0800

    add global exception handler

---
+++ mist-common/src/main/java/com/wanglongbiao/mist/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.wanglongbiao.mist;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Log4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({ServiceException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class})
+    public Map<String, Object> handleException(Exception e) {
+        Map<String, Object> map = new HashMap<>(3);
+        map.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        map.put("data", Collections.emptyMap());
+        if (e instanceof MethodArgumentNotValidException) {
+            String message = ((MethodArgumentNotValidException) e).getBindingResult().getFieldError().getDefaultMessage();
+            map.put("message", message);
+        } else {
+            map.put("message", e.getMessage());
+        }
+//        log.error(e.getMessage().toString());
+        return map;
+    }
+}
---
+++ mist-common/src/main/java/com/wanglongbiao/mist/ServiceException.java
@@ -0,0 +1,4 @@
+package com.wanglongbiao.mist;
+
+public class ServiceException extends RuntimeException {
+}
