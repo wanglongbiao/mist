5c1649b3d1f9dbc68642900b50af999191fdfd49
wanglongbiao <wanglongbiao@vip.qq.com>
2022-01-09 18:50:02 +0800

    config eureka server

--- mist-eureka-server/src/main/java/com/wanglongbiao/mist/eureka/server/MistEurekaServerApplication.java
+++ mist-eureka-server/src/main/java/com/wanglongbiao/mist/eureka/server/MistEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.wanglongbiao.mist.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MistEurekaServerApplication {
     public static void main(String[] args) {
