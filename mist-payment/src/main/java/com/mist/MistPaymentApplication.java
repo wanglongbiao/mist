ec19f12265fdf09f59e13f9526cc037a9e0d505d
wanglongbiao <wanglongbiao@vip.qq.com>
2022-01-09 21:56:45 +0800

    delete eureka and add zookeeper

--- mist-payment/src/main/java/com/mist/MistPaymentApplication.java
+++ mist-payment/src/main/java/com/mist/MistPaymentApplication.java
@@ -2,9 +2,9 @@ package com.mist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MistPaymentApplication {
     public static void main(String[] args) {
