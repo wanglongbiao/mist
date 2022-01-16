92750fddcd3ad7ca64cdb12745e83a3ce1f5838d
wanglongbiao <wanglongbiao@vip.qq.com>
2022-01-16 12:55:58 +0800

    add config module

---
+++ mist-config/src/main/java/com/mist/mistconfig/MistConfigApplication.java
@@ -0,0 +1,13 @@
+package com.mist.mistconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MistConfigApplication.class, args);
+    }
+
+}
---
+++ mist-config/src/test/java/com/mist/mistconfig/MistConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mist.mistconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
